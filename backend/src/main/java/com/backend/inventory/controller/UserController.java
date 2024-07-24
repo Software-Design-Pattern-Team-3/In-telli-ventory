c73300a031100c15edb8c670e9efe1bd046e1d64
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2024-07-24 14:59:16 +0530

    Update UserController.java

--- backend/src/main/java/com/backend/inventory/controller/UserController.java
+++ backend/src/main/java/com/backend/inventory/controller/UserController.java
@@ -4,16 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.backend.inventory.exception.EmailAlreadyRegisteredException;
-import com.backend.inventory.model.User;
-import com.backend.inventory.service.UserService;
-
-import io.micrometer.core.ipc.http.HttpSender.Response;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,10 +13,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.inventory.exception.EmailAlreadyRegisteredException;
+import com.backend.inventory.model.User;
+import com.backend.inventory.service.UserService;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin("http://localhost:5173")
 public class UserController {
 
     @Autowired
