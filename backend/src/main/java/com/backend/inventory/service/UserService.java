8b025fb0a8c19c33d8e41543f7d3e5c9a0557f5b
VIGNESH VASU <vigneshvasu0511@gmail.com>
2024-07-24 14:10:16 +0530

    update user mvc with put ,delete and mail reg config

--- backend/src/main/java/com/backend/inventory/service/UserService.java
+++ backend/src/main/java/com/backend/inventory/service/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.backend.inventory.exception.EmailAlreadyRegisteredException;
 import com.backend.inventory.model.User;
 import com.backend.inventory.repository.UserRepository;
 
@@ -19,12 +20,43 @@ public class UserService {
     }
 
     public User createUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new EmailAlreadyRegisteredException(user.getEmail());
+        }
         return userRepository.save(user);
     }
 
     public void deleteAllUsers() {
         
         userRepository.deleteAll();
+    }
+
+    public User getUser(long id) {
+        return userRepository.findById(id).orElse(null);
+        
+    }
+
+    public User updateUser(long id, User user) {
+        
+        User existingUser = userRepository.findById(id).orElse(null);
+        existingUser.setFirstname(user.getFirstname());
+        existingUser.setLastname(user.getLastname());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPassword(user.getPassword());
+
+        return userRepository.save(existingUser);
+
+        
+    }
+
+    public String deleteById(long id) {
+
+        if(!userRepository.existsById(id))
+        {
+            return "user with Id:"+id+" does not exists";
+        }
+        userRepository.deleteById(id);
+        return "user with id "+id+" has been deleted success.";
     }
 }
 
