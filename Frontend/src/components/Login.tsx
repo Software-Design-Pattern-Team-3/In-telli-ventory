5dfa63837c8665efffb22e71507144266dbcedf4
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2024-07-26 10:27:00 +0530

    Co-authored-by: vignesh2004vasu vigneshvasu0511@gmail.com
    landing page, dark mode, light mode

--- Frontend/src/components/Login.tsx
+++ Frontend/src/components/Login.tsx
@@ -20,7 +20,7 @@ function Signin() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [errors, setErrors] = useState({ email: "", password: "" });
-  // const navurl = import.meta.env.VITE_REACT_APP_API_URL
+ 
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -96,7 +96,7 @@ function Signin() {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
       <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-        <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">Welcome to Intelliventory</h2>
+        <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">Welcome to StockSync</h2>
         <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
           Login to view inventory status and analytics.
         </p>
