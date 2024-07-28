88a20165c79d910c9a476ceca14769b482d53a8c
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2024-07-28 23:00:47 +0530

    Chart edits

--- Frontend/src/components/ui/pie-chart.tsx
+++ Frontend/src/components/ui/pie-chart.tsx
@@ -60,8 +60,8 @@ export function PieChartDash() {
   return (
     <Card className="flex flex-col">
       <CardHeader className="items-center pb-0">
-        <CardTitle>Pie Chart - Donut with Text</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
+        <CardTitle>Demand Forecast</CardTitle>
+        <CardDescription>July - November 2024</CardDescription>
       </CardHeader>
       <CardContent className="flex-1 pb-0">
         <ChartContainer
@@ -115,10 +115,10 @@ export function PieChartDash() {
       </CardContent>
       <CardFooter className="flex-col gap-2 text-sm">
         <div className="flex items-center gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+          Will trend up by 13% in coming months <TrendingUp className="h-4 w-4" />
         </div>
         <div className="leading-none text-muted-foreground">
-          Showing total visitors for the last 6 months
+          Predicting total visitors for the next 6 months
         </div>
       </CardFooter>
     </Card>
